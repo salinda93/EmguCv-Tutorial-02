3d7a5e6ed09582d1b76dc932691c555fd5017886
Salinda <salinda.jayasundara3010@gmail,com>
2020-05-16 06:28:53 +0530

    final commit

---
+++ EmguCv Basic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmguCv_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
